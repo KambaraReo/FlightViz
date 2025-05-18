5c31123143f9424041a4b457028997c89975050c
KambaraReo <r.kambara1997@gmail.com>
2025-05-18 19:30:38 +0900

    Initialize monorepo with backend and frontend

---
+++ frontend/vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  // localhost:5173 に外部（Dockerホスト）からアクセスできる状態にするために以下を追加
+  server: {
+    host: '0.0.0.0',
+    port: 5173
+  }
+})
